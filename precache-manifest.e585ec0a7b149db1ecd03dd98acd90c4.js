79b0a9a58fe7900a175d0a1ed8a726928ee45b99
slavarikus <39729510+slavarikus@users.noreply.github.com>
2020-01-29 19:27:08 +0300

    Updates

---
+++ precache-manifest.e585ec0a7b149db1ecd03dd98acd90c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179435acb1e360cf0ac83e09c70edae5",
+    "url": "/quick-exchange/index.html"
+  },
+  {
+    "revision": "1a2ef7335d2c3c83f80b",
+    "url": "/quick-exchange/static/css/main.93af0fd9.chunk.css"
+  },
+  {
+    "revision": "3f53e5fb637e09037544",
+    "url": "/quick-exchange/static/js/2.85f1d1ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quick-exchange/static/js/2.85f1d1ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a2ef7335d2c3c83f80b",
+    "url": "/quick-exchange/static/js/main.20abedd1.chunk.js"
+  },
+  {
+    "revision": "d5bee0522e3c2f1859cc",
+    "url": "/quick-exchange/static/js/runtime-main.c855060c.js"
+  }
+]);
